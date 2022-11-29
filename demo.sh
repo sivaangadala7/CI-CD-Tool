fa5a294df712fda06c389c6d108e14babab483b8
sivaangadala7 <96822765+sivaangadala7@users.noreply.github.com>
2022-11-29 17:07:37 +0530

    Add files via upload

---
+++ demo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "*************display root f.s space*************"
+df -h
+
+echo"************display today date****************"
+date
+
+echo "*************display jenkins memory space*********"
+free -m
+
